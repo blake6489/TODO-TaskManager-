711928506cf6566aacecbcc29f41c49463ea3665
Blake Eggemeyer <i.linwin@gmail.com>
2011-11-26 09:12:59 -0900

    cutting code

---
+++ code/main.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+using namespace std;
+
+#include "tools/utils.cpp"
+
+
+int main (int argc, char* argv[])
+{
+	
+
+	return 0;
+}
